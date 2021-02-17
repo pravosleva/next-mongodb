22a97c588a4acedf1c38bd17ce61f291fefb0961
Den <selection4test@gmail.com>
2021-02-17 13:17:25 +0300

    lets try on prod

--- express-tools/e-api/common/index.js
+++ express-tools/e-api/common/index.js
@@ -4,9 +4,15 @@ const router = express()
 router.get('/my-ip', async (req, res) => {
   try {
     const ip = req.clientIp
+    const ipFromHeader = req.headers['x-real-ip']
     const geo = req.geo
 
-    res.json({ ip, geo, success: true })
+    res.json({
+      ip,
+      ipFromHeader,
+      geo,
+      success: true,
+    })
   } catch (e) {
     res.status(500).send({
       message: `Error: ${e?.message || '(no err msg)'}`,
