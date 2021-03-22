4c888750be8cca0fc9282e58e41e7298846e0cc3
Den <selection4test@gmail.com>
2021-03-22 17:33:32 +0300

    fix

--- common/react-markdown-renderers/index.ts
+++ common/react-markdown-renderers/index.ts
@@ -1,4 +1,4 @@
-import { Alert } from './Alert'
+// import { Alert } from './Alert'
 import { BlockquoteRenderer } from './BlockquoteRenderer'
 import { CodeRendererSynthwave84, CodeRendererMaterialOceanic } from './CodeRenderer'
 import { HeadingRenderer } from './HeadingRenderer'
