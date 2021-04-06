674f43ea25bfa42e81d287f5c39669a4e00fe62b
Den <selection4test@gmail.com>
2021-04-06 19:12:26 +0300

    fix

--- common/components/PinnedNotes/PinnedNotes.tsx
+++ common/components/PinnedNotes/PinnedNotes.tsx
@@ -2,20 +2,33 @@ import { useMemo, useCallback } from 'react'
 import { useGlobalAppContext, useNotifsContext } from '~/common/hooks'
 
 export const PinnedNotes = () => {
-  const { pinnedIds, state } = useGlobalAppContext()
+  const { pinnedIds, state, handleSetAsActiveNote } = useGlobalAppContext()
   const { addDefaultNotif } = useNotifsContext()
   const { activeNote } = useMemo(() => state, [JSON.stringify(state)])
-  const handleClick = useCallback((id: string) => {
-    addDefaultNotif({
-      title: 'TODO',
-      message: (
-        <ul style={{ paddingLeft: '10px' }}>
-          <li>Get note by id: {id}</li>
-          <li>Set as Active Note</li>
-        </ul>
-      ),
-    })
-  }, [])
+  const handleClick = useCallback(
+    (id: string) => {
+      if (!!state.activeNote && state.activeNote?._id === id) {
+        // addDefaultNotif({ title: 'Unnecessary', message: 'Note is active' })
+        return
+      }
+
+      const fromState = state.notes.find(({ _id }: any) => _id === id)
+      if (!!fromState) {
+        handleSetAsActiveNote(fromState)
+      } else {
+        addDefaultNotif({
+          title: 'TODO',
+          message: (
+            <ul style={{ paddingLeft: '10px' }}>
+              <li>Get note by id: {id}</li>
+              <li>Set as Active Note</li>
+            </ul>
+          ),
+        })
+      }
+    },
+    [JSON.stringify(state.notes), state.activeNote, handleSetAsActiveNote]
+  )
 
   if (pinnedIds.length === 0) return <em>No pinned notes yet...</em>
   return (
